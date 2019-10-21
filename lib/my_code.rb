0c8c2d6ffe3aebcb1d6444a6d767b0a507afa921
Matt Bechtel <bechma02@gmail.com>
2019-10-21 23:39:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
-# My Code here....
+def map_to_negativize(source_array)
+  result_array = []
+
+  i = 0
+  while i < source_array.length do
+    result_array[i] = source_array[i]*(-1)
+    i += 1
+  end
+
+  result_array
+
+  # My Code here....
